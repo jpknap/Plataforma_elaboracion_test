dbaaab5499dd8f8f207fb6d6b1977c3bfaa00df0
jpknap <jp.knap@gmail.com>
2015-07-20 11:28:33 -0300

    gestion prueba 1/2

---
+++ js/gestionPrueba.js
@@ -0,0 +1,133 @@
+var paginaActual=1;
+var tituloBuscar='';
+var listaTags = [];
+$(document).ready(function()
+		{
+			tituloBuscar=$("#tituloPreguntaBuscar").val();
+			$.post('php/prueba.php',{operacion:1,dat1:1,dat2:tituloBuscar,dat3:JSON.stringify(listaTags),dat4:localStorage.idUser } ,procesarLecturaXML,'json');
+			$.post('php/tag.php',{operacion:1,dat1:'',dat2:"prueba",dat3:'ALL',dat4:localStorage.idUser},function(data){
+				 var codeTags='';
+				 $.each(data, function(name, info){
+					codeTags+='<option value="'+info.id+'">'+info.nombre+'</option>';
+				 });
+				 $("#tagPreguntaBuscar").html(codeTags);
+			},'json');
+
+
+});
+
+function editarPreg(idPreg,url){
+	console.log("edicion");
+	/*sessionStorage.setItem('idPreguntaEdit',idPreg);
+	sessionStorage.setItem('urlPreguntaEdit',url);
+	window.location.href='editarPregunta.html';*/
+}
+function cargarDescripcion(titulo,descripcion){
+
+	$("#textAdicionalPreview").html(decodeURIComponent(titulo));
+	$('#preguntaPreview').html(decodeURIComponent(descripcion));
+	$('#velo').fadeIn(500);
+}
+function filtrarPreg(){
+	tituloBuscar=$("#tituloPreguntaBuscar").val();
+	paginaActual=1;
+	$.post('php/prueba.php',{operacion:1,dat1:1,dat2:tituloBuscar,dat3:JSON.stringify(listaTags),dat4:localStorage.idUser },procesarLecturaXML,'json');
+}
+
+function asignarTag(){
+	var idTag=$("#tagPreguntaBuscar").val();
+	var nombreTag=$("#tagPreguntaBuscar option:selected").text();
+	var result = $.grep(listaTags, function(e){ return e.id == idTag; });
+	if(result.length == 0){
+		listaTags.push({id:idTag,nombre: nombreTag});
+		$("#listaTags").append('<li><a onClick="removerTag('+(listaTags.length-1)+')" class="tag">'+listaTags[listaTags.length-1].nombre+'</a></li>');
+	}
+	else
+		mensajeError("No puedes repetir el Tag a un mismo filtro.");
+}
+function removerTag(posTag){
+	listaTags.splice(posTag,1);
+	var codeTags='';
+	for(var i =0 ;i<listaTags.length ; i++){
+	 codeTags+= '<li><a onClick="removerTag('+i+')" class="tag">'+listaTags[i].nombre+'</a></li>';
+	}
+	$("#listaTags").html(codeTags)
+}
+
+function procesarLecturaXML(data){
+		var preguntas="<tr><th>Titulo</th><th>Descripcion</th><th>Fecha</th><th>Editar</th><th>Eliminar</th></tr>";
+		 $.each(data, function(name, info){
+		 	if(name != 'cantidad'){
+		 		preguntas +='<tr><td width=350 >'+info.titulo+'</td><td><a  style="cursor:pointer;" onClick=cargarDescripcion("'+encodeURIComponent(info.titulo)+'","'+encodeURIComponent(info.descripcion)+'");><img height="30" width="30" src="images/lupa.png"> </img></a><td>'+info.fecha+'</td></td><td><a  style="cursor:pointer;" onClick=editarPrueba("'+info.id+'");> <img height="30" width="30" src="images/edit.png"> </img></a></td><td><button class="button_rojo">Eliminar</button></td> </tr>';
+		 	}
+		 	else{
+		 		paginador(info.value);
+		 	}
+		 });
+		 $("#tablaPreguntas").html(preguntas);
+}
+function ultimasPaginas(numero,nPag){
+	var string='';
+	var codeHTML=[]
+		for(var j = nPag-(5-numero); j > 0 && j >= paginaActual-numero; j--){
+				codeHTML.push('<li><a href="#" onClick="llamadaPhpPagina('+j+');">'+j+'</a></li>');
+	}
+	string+= codeHTML.reverse().toString().replace(/,/g,'');
+	string+= '<li class="active">'+paginaActual+'</li>';
+	for(var i = 1; i <= 4-numero ; i++){
+		string +='<li><a href="#" onClick="llamadaPhpPagina('+(i+paginaActual)+');">'+(i+paginaActual)+'</a></li>';
+	}
+	return string;
+}
+
+function paginador (cant){
+	
+	var htmlPag ='';
+	var nPag= Math.ceil(cant/10);
+	if(paginaActual > 1){
+		htmlPag+='<li class="next"><a href="#"  onClick="llamadaPhpPagina(1);">««</a></li>';
+		htmlPag+= '<li class="previous"><a href="#"  onClick="llamadaPhpPagina('+(paginaActual-1)+');"> «Anterior</a> </li>';
+		
+	}
+	else{
+
+		htmlPag+='<li class="next-off">««</li>';
+		htmlPag+= '<li class="previous-off"> «Anterior </li>';
+	}
+	
+	
+	//htmlPag+= '<li class="active">'+paginaActual+'</li>';
+	for(var i =paginaActual; i <= (paginaActual+4) && i <= nPag ; i++){
+
+		if((nPag-paginaActual)<5)  {			
+			htmlPag+= ultimasPaginas(4-(nPag-paginaActual),nPag);
+			break;
+		}
+		else
+			if(i == paginaActual)		
+				htmlPag+= '<li class="active">'+i+'</li>';
+			else
+				htmlPag+= '<li><a href="#" onClick="llamadaPhpPagina('+i+');">'+i+'</li>';
+	}
+	if(paginaActual < nPag){
+  		htmlPag+='<li class="next"><a href="#"  onClick="llamadaPhpPagina('+(paginaActual+1)+');">Siguiente» </a></li>';               
+  		htmlPag+='<li class="next"><a  href="#" onClick="llamadaPhpPagina('+nPag+');">»»</a></li>';}
+  	else {
+  		htmlPag+='<li class="next-off">Siguiente» </li>';
+  		htmlPag+='<li class="next-off">»»</li>';}               
+  	
+
+  $("ul#pagination-flickr").html(htmlPag);
+
+}
+function llamadaPhpPagina(numero){
+	paginaActual=numero;
+	$.post('php/prueba.php',{operacion:1,dat1:numero,dat2:tituloBuscar,dat3:JSON.stringify(listaTags)},procesarLecturaXML,'json');
+
+}
+function mensajeError(texto){
+	if(!$("#notificacion_top_error").is(':visible')){
+		$("#notificacion_top_error").text(texto);
+		$("#notificacion_top_error").show(500).delay(1000).fadeOut();
+	}
+};
--- php/prueba.php
+++ php/prueba.php
@@ -3,6 +3,7 @@ include "conexionBD.php";
 if($_POST['operacion'] !=""){
 	switch($_POST['operacion']){
 		case 0: generarPrueba($_POST['dat1'],$_POST['dat2'],$_POST['dat3'],$_POST['dat4'],$_POST['dat5']); break;
+		case 1: cargarPrueba($_POST['dat1'],$_POST['dat2'],$_POST['dat3'],$_POST['dat4']); break;
 	}
 }
 function generarPrueba($idUser, $titulo, $descripcion, $tags, $listaPreguntas){
@@ -35,4 +36,41 @@ function agregarPreguntaPrueba($id,$listaPreguntas){
 		}
 		
 }
+
+function cargarPrueba($hoja,$titulo,$tags,$idUser){
+		global $conexion;
+		$dataTag = json_decode($tags,true);
+		$tagSql='';
+		$cantTag=count($dataTag);		
+		$tagSql2='';
+		if($cantTag>0){
+
+			$tagSql2='having count(*)='.$cantTag;
+			$tagSql='AND a.id=prueba_tag.id_prueba AND  prueba_tag.id_tag in (';
+			for($i=0;$i<$cantTag;$i++){
+				if($i <$cantTag-1)
+					$tagSql .= ''.$dataTag[$i]['id'].',';
+				else
+					$tagSql .= ''.$dataTag[$i]['id'].') ';
+			};
+		}
+		$data = array();
+		$cant_Hoja=10;
+		$sql_id = 'SELECT a.id as id FROM prueba a  left JOIN prueba_tag  on 1 WHERE a.id_usuario='.$idUser.' AND a.titulo LIKE "%'.$titulo.'%" '.$tagSql.' GROUP BY id '.$tagSql2;
+		  // <---- OJO! 
+	
+		$query1 = mysql_query($sql_id,$conexion) or die ("Error in query: $query. ".mysql_error());
+		$data['cantidad']=array("value"=>mysql_num_rows($query1));
+
+		$sql_id = 'SELECT id as id , titulo as titulo, descripcion as descripcion ,date_format(fechaCreacion,"%d-%m-%Y") as fecha  FROM prueba a  left JOIN prueba_tag  on 1 WHERE a.id_usuario='.$idUser.'  and a.titulo LIKE "%'.$titulo.'%" '.$tagSql.'  GROUP BY id '.$tagSql2.' order by fechaCreacion DESC LIMIT '.$cant_Hoja*($hoja-1).','.$cant_Hoja.' ;';  // <---- OJO! 
+		$query1 = mysql_query($sql_id,$conexion) or die ("Error in query: $query. ".mysql_error());
+		//$resp=mysql_result($query1);
+		while ($row = mysql_fetch_assoc($query1, MYSQL_ASSOC)) {
+			$data[]=array("id"=>$row['id'],"titulo"=>$row['titulo'],"descripcion"=>$row['descripcion'],"fecha"=>$row['fecha']);
+		}
+		header("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
+		header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Fecha en el pasado
+		Header("Content-type: application/json");
+		echo json_encode($data);
+	}
 ?>
