794c0cc1105ebc6d9b9d695bbcfdaa49e0cc2751
jpknap <jp.knap@gmail.com>
2015-07-19 21:09:46 -0300

    creacion de pruebas

--- js/crearPrueba.js
+++ js/crearPrueba.js
@@ -12,7 +12,24 @@ $(function()
 			 });
 			 $("#seleccionTag").html(codeTags);
 		},'json');
+		cargarTagsBusqueda();
 });
+
+function mensajeError(texto){
+	if(!$("#notificacion_top_error").is(':visible')){
+		$("#notificacion_top_error").text(texto);
+		$("#notificacion_top_error").show(500).delay(1000).fadeOut();
+	}
+};
+//generacion de prueba 
+function generarPrueba(){
+
+	var titulo =$("#tituloPregunta").val();
+	var descripcion= $("#idTextoAdicional").val();
+	$.post('php/prueba.php',{operacion:0,dat1:localStorage.idUser,dat2:titulo,dat3:descripcion,dat4:JSON.stringify(listaTags),dat5:JSON.stringify(listaPreguntas) },procesarLecturaXML,'json');
+}
+//fin funciones generacion de prueba
+//funciones tag pruebas 
 function asignarTag(){
 	var idTag=$("#seleccionTag").val();
 	var nombreTag=$("#seleccionTag option:selected").text();
@@ -24,12 +41,6 @@ function asignarTag(){
 	else
 		mensajeError("No puedes repetir el Tag a una misma pregunta.");
 }
-function mensajeError(texto){
-	if(!$("#notificacion_top_error").is(':visible')){
-		$("#notificacion_top_error").text(texto);
-		$("#notificacion_top_error").show(500).delay(1000).fadeOut();
-	}
-};
 function removerTag(posTag){
 	listaTags.splice(posTag,1);
 	var codeTags='';
@@ -38,7 +49,7 @@ function removerTag(posTag){
 	}
 	$("#listaTags").html(codeTags)
 }
-
+// fin funciones tag prueba
 function procesarLecturaXML(data){
 		var preguntas='<tr><th width="20%" >CODE</th><th width="50%">Titulo</th><th width="10%">Preview</th><th width="20%">Asignar</th></tr>';
 		 $.each(data, function(name, info){
@@ -84,12 +95,59 @@ function filtrarPreg(){
 	paginaActual=1;
 	$.post('php/cargarPreguntas.php',{operacion:0,dat1:1,dat2:tituloBuscar,dat3:JSON.stringify(listaTagsBusqueda),dat4:localStorage.idUser },procesarLecturaXML,'json');
 }
+///funciones a la gestion y asignacion de preguntas de una prueba
 function asignarPregunta(titulo,id, url){
-	console.log(id);
-	$("#preguntasAsig").append('<div style="background-color:yellow; margin-top:4px; height: 45px; width: 100%;"> <snap style=" margin-left:7px;  width:110px; float:left; ">'+decodeURI(titulo).substring(0,15)+'</snap> <a  style="cursor:pointer; margin-top:10px; float:left;" onClick=cargarVelo("'+url+'");><img height="25" width="25" src="images/lupa.png"> </img></a> <button style=" margin-left:5px;  margin-top:10px;float:left;"> X </button> </div>');
-	
+	//valida que no se repita el id de un pregunta
+	var result = $.grep(listaPreguntas, function(e){ return e.id == id; });
+	if(result.length == 0){
+		listaPreguntas.push({id:id,titulo: titulo, url:url});
+		$("#preguntasAsig").append('<div style="background-color:yellow; margin-top:4px; height: 45px; width: 100%;"> <snap style=" margin-left:7px;  width:110px; float:left; ">'+decodeURI(titulo).substring(0,15)+'</snap> <a  style="cursor:pointer; margin-top:10px; float:left;" onClick=cargarVelo("'+url+'");><img height="25" width="25" src="images/lupa.png"> </img></a> <button style=" margin-left:5px;  margin-top:10px;float:left;" onclick=quitarPregunta('+(listaPreguntas.length-1)+')> X </button> </div>');
+	}
+	else{
+		console.log("no se puede repetir pregunta");
+	}
 }
-//paginacion de elementos pregunta
+
+function quitarPregunta(posPregunta){
+	listaPreguntas.splice(posPregunta,1);
+	codePreguntaList ='';
+	for(var i =0 ;i<listaPreguntas.length ; i++){
+	 codePreguntaList+= '<div style="background-color:yellow; margin-top:4px; height: 45px; width: 100%;"> <snap style=" margin-left:7px;  width:110px; float:left; ">'+decodeURI(listaPreguntas[i].titulo).substring(0,15)+'</snap> <a  style="cursor:pointer; margin-top:10px; float:left;" onClick=cargarVelo("'+listaPreguntas[i].url+'");><img height="25" width="25" src="images/lupa.png"> </img></a> <button style=" margin-left:5px; margin-top:10px;float:left;" onclick="quitarPregunta('+i+')" > X </button> </div>';
+	}
+	$("#preguntasAsig").html(codePreguntaList)
+}
+//fin funciones relacionadas con las preguntas contenidas de una prueba
+
+//funciones tag busqueda
+function cargarTagsBusqueda(){
+	$.post('php/tag.php',{operacion:1,dat1:'',dat2:"pregunta",dat3:'ALL',dat4:localStorage.idUser},function(data){
+		 var codeTags='';
+		 $.each(data, function(name, info){
+			codeTags+='<option value="'+info.id+'">'+info.nombre+'</option>';
+		 });
+		 $("#tagPreguntaBuscar").html(codeTags);
+	},'json');
+}
+function asignarTagBusqueda(){
+	var id=$("#tagPreguntaBuscar").val();
+	var nombreTag=$("#tagPreguntaBuscar option:selected").text();
+	var result = $.grep(listaTagsBusqueda, function(e){ return e.id == id; });
+	if(result.length == 0){
+		listaTagsBusqueda.push({id:id,nombre: nombreTag});
+		$("#listaTagsBusqueda").append('<li><a onClick="removerTagBusqueda('+(listaTagsBusqueda.length-1)+')" class="tag">'+listaTagsBusqueda[listaTagsBusqueda.length-1].nombre+'</a></li>');
+	}
+	else
+		mensajeError("No puedes repetir el Tag a una misma pregunta.");
+}
+function removerTagBusqueda(posTag){
+	listaTagsBusqueda.splice(posTag,1);
+	var codeTags='';
+	for(var i =0 ;i<listaTagsBusqueda.length ; i++){
+	 codeTags+= '<li><a onClick="removerTagBusqueda('+i+')" class="tag">'+listaTagsBusqueda[i].nombre+'</a></li>';
+	}
+	$("#listaTagsBusqueda").html(codeTags)
+}
+//fin funciones tag busqueda
 
 
 function ultimasPaginas(numero,nPag){
--- php/prueba.php
+++ php/prueba.php
@@ -2,5 +2,37 @@
 include "conexionBD.php";
 if($_POST['operacion'] !=""){
 	switch($_POST['operacion']){
+		case 0: generarPrueba($_POST['dat1'],$_POST['dat2'],$_POST['dat3'],$_POST['dat4'],$_POST['dat5']); break;
 	}
 }
+function generarPrueba($idUser, $titulo, $descripcion, $tags, $listaPreguntas){
+		global $conexion;
+	$sql= 'INSERT INTO prueba(id_usuario,titulo,descripcion,fechaCreacion) VALUES ('.$idUser.',"'.$titulo.'","'.$descripcion.'",'.'"'.date("Y-m-d H:i:s").'");';
+	if (mysql_query($sql,$conexion)){
+		$id = mysql_insert_id($conexion);
+		agregarTagPrueba($id,$tags);
+		agregarPreguntaPrueba($id,$listaPreguntas);
+
+	}
+}
+function agregarTagPrueba($id,$tags){
+		global $conexion;
+		$data = json_decode($tags,true);
+		$sql='';
+		for($i=0;$i<count($data);$i++){
+			$sql='INSERT INTO prueba_tag(id_tag, id_prueba) VALUES ('.$data[$i]['id'].','.$id.');';
+			mysql_query($sql,$conexion);
+		}
+		
+}
+function agregarPreguntaPrueba($id,$listaPreguntas){
+		global $conexion;
+		$data = json_decode($listaPreguntas,true);
+		$sql='';
+		for($i=0;$i<count($data);$i++){
+			$sql='INSERT INTO pregunta_prueba(id_pregunta, id_prueba) VALUES ('.$data[$i]['id'].','.$id.');';
+			mysql_query($sql,$conexion);
+		}
+		
+}
+?>
